c63da55e6976b6e18bdca92cb7ea3c9272ecc9da
Yuhao Lin <lyh970817@yandex.com>
2021-03-03 01:15:45 +0800

    start-up commit

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ln -s $(pwd)/config/* $HOME/.config/
-# ln -s $(pwd)/bin/* $HOME/.local/bin/
+ln -s $(pwd)/config/* $HOME/.config/
+ln -s $(pwd)/bin/* $HOME/.local/bin/
 
 ln -s $(pwd)/dotfiles/profile $HOME/.profile
 ln -s $(pwd)/dotfiles/Xdefaults2 $HOME/.Xdefaults
@@ -13,13 +13,15 @@ ln -s $(pwd)/dotfiles/gitconfig $HOME/.gitconfig
 sudo ln -s $(pwd)/etc/zsh/zshenv /etc/zsh/zshenv
 
 
-# mkdir $HOME/.tmp
-# rm $HOME/.bash_logout $HOME/.bash_profile
+mkdir $HOME/.tmp
+rm $HOME/.bash_logout $HOME/.bash_profile
 
-# sudo rm /usr/share/gtk-2.0/gtkrc
-# sudo ln -s ~/.config/gtk-2.0/gtkrc-2.0 /usr/share/gtk-2.0/gtkrc
-# sudo usermod -a -G video $USER
-# sudo timedatectl set-ntp true
-# sudo sed -i 's/AutoEnable=false/AutoEnable=true/' /etc/bluetooth/main.conf
+sudo rm /usr/share/gtk-2.0/gtkrc
+sudo ln -s ~/.config/gtk-2.0/gtkrc-2.0 /usr/share/gtk-2.0/gtkrc
+sudo usermod -a -G video $USER
+sudo timedatectl set-ntp true
+sudo sed -i 's/AutoEnable=false/AutoEnable=true/' /etc/bluetooth/main.conf
 
-# xdg-mime default org.pwmt.zathura-pdf-mupdf.desktop application/pdf
+xdg-mime default org.pwmt.zathura-pdf-mupdf.desktop application/pdf
+
+sudo v2gen -u https://1.mxxst2.de/link/IXkdaCmjaXsAz6RG?sub=3&extend=1 --best
