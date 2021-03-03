590693fb418389563caafa86f53698e48c784036
Yuhao Lin <lyh970817@yandex.com>
2021-03-03 09:05:14 +0800

    start-up commit

---
+++ install/other_installs.sh
@@ -0,0 +1,9 @@
+sh ./pamac.sh
+
+git clone https://github.com/iochen/v2gen/ && cd v2gen
+env GOPRIVATE=github.com/v2ray/v2ray-core go build ./cmd/v2gen
+
+# Install st
+git clone https://github.com/LukeSmithxyz/st
+cd st
+sudo make install
