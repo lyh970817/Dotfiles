cc7abf1930be9e14fafd7d2ad15a2c038c6f6d3c
Yuhao Lin <lyh970817@yandex.com>
2021-03-10 22:52:51 +0800

    Update

--- install/other_installs.sh
+++ install/other_installs.sh
@@ -1,6 +1,5 @@
-sudo wget https://github.com/iochen/v2gen/releases/download/v2.0.1/v2gen_386_linux -P /usr/local/bin -O v2gen
-sudo chmod 755 /usr/local/bin/v2gen
 sudo cp ~/.dotfiles/install/st /usr/local/bin
+sudo cp ~/.dotfiles/install/v2gen /usr/local/bin
 
 sudo pip install jupyter-notebook
 sudo pip install ipython
