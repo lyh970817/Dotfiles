9e134f62c5e256cd915fe2d0020bb7d23055a038
Yuhao Lin <lyh970817@yandex.com>
2021-03-17 21:56:20 +0800

    Scripted auto-commit on change (2021-03-17 21:56:20) by gitwatch.sh

--- config.sh
+++ config.sh
@@ -20,7 +20,7 @@ ln -s $(pwd)/dotfiles/curlrc $HOME/.curlrc
 sudo ln -s $(pwd)/etc/zsh/zshenv /etc/zsh/zshenv
 sudo ln -s $(pwd)/etc/pkgsync/pkg_install.list /etc/pkgsync/pkg_install.list
 
-ln -s
+ln -s ~/Yandex.Disk/.tmp ~/.tmp
 
 
 sudo rm /usr/share/gtk-2.0/gtkrc
